a802b515ffccb799f7afa12e29824ecd1695243c
BHUTUU <raj259942@gmail.com>
2021-10-20 10:28:09 +0530

    quickQuiz quesion of chapter 2 solved

---
+++ quickQuiz.java
@@ -0,0 +1,24 @@
+package com.bhutuu;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+   double a, b, c, d, e, mean, zero, kine, simpl;
+   Scanner bhu = new Scanner(System.in);
+   System.out.print("Enter a: ");
+   a = bhu.nextInt();
+   System.out.print("Enter b: ");
+   b = bhu.nextInt();
+   System.out.print("Enter c: ");
+   c = bhu.nextInt();
+   System.out.print("Enter v: ");
+   d = bhu.nextInt();
+   System.out.print("Enter u: ");
+   e = bhu.nextInt();
+   mean=a/2-b/2;
+   System.out.println("a-b/2 is equal to: " + mean );
+   zero=b*b/2/a-2*c;
+   System.out.println("(bsq-4ac) "+ zero);
+   kine=d*d-e*e;
+   System.out.println("v^2 - u^2 is equal to: "+ kine);
+  }
+}
---
+++ rough/pr2.java
@@ -0,0 +1,14 @@
+package com.bhutuu;
+public class Main {
+  public static void main(String[] args) {
+    double a=2, b=3, c=4, x=5, y=6, u=4, v=5, d=9;
+    double var1=x/2-y/2;
+    double var2=b*b/2/a-2*c;
+    double var3=v*v-u*u;
+    double var4=a*b-d;
+    System.out.println("(5-6)/2 is: "+ var1);
+    System.out.println("(b.sq - 4ac)/2a: "+var2);
+    System.out.println("v.sq - u.sq: "+var3);
+    System.out.println("a*b-d: "+var4);
+  }
+}
