4dbb2957d634abc78609fd9cd94f9749613ac15d
BHUTUU <raj259942@gmail.com>
2021-11-23 13:32:13 +0530

    some java practice files

--- quickQuiz.java
+++ quickQuiz.java
@@ -10,15 +10,17 @@ public class Main {
    b = bhu.nextInt();
    System.out.print("Enter c: ");
    c = bhu.nextInt();
-   System.out.print("Enter v: ");
+   System.out.print("Enter v or d: ");
    d = bhu.nextInt();
    System.out.print("Enter u: ");
    e = bhu.nextInt();
    mean=a/2-b/2;
    System.out.println("a-b/2 is equal to: " + mean );
    zero=b*b/2/a-2*c;
-   System.out.println("(bsq-4ac) "+ zero);
+   System.out.println("((bsq-4ac)/2a) "+ zero);
    kine=d*d-e*e;
    System.out.println("v^2 - u^2 is equal to: "+ kine);
+   simpl=a*b-d;
+   System.out.println("ab - d is: "+ simpl);
   }
 }
