ff58e9f62602fd3b828508c6b3714b7419a98775
Suman Kumar ~BHUTUU <raj259942@gmail.com>
2022-01-13 22:01:04 +0530

    resistor main file updated

--- resistorCalculate/ResistorColorCode.java
+++ resistorCalculate/ResistorColorCode.java
@@ -10,6 +10,7 @@ public class ResistorColorCode {
   public static void main(String[] args) {
     int a, back=0, brown=1, red=2, orange=3, yellow=4, green=5, blue=6, violet=7, grey=8, white=9;
     String firstColor, secondColor, thirdColor, fourthColor,  stValue, value;
+    String validColors="black brown red orange yellow green blue violet grey white";
     Scanner bhu = new Scanner(System.in);
     Scanner st= new Scanner(System.in);
     System.out.println("""
@@ -54,6 +55,49 @@ public class ResistorColorCode {
         } else {
           System.out.println("Enter with a valid unit! either o or k");
         }
+      } else if (opt == 2) {
+        System.out.print("How  many colorBands on your resistor? (four or five)=> ");
+        int band=bhu.nextInt();
+        System.out.println(band);
+        if (band==4) {
+          System.out.println("Valid colors: black, brown, red, orange, yellow, green, blue, violet, grey, white\n");
+          System.out.print("Enter the first band color=>");
+          String a1=st.nextLine();
+          System.out.print("Enter the Second band color=>");
+          String a2=st.nextLine();
+          System.out.print("Enter the third band color=>");
+          String a3=st.nextLine();
+          firstColor=a1.toLowerCase();
+          secondColor=a2.toLowerCase();
+          thirdColor=a3.toLowerCase();
+          fourthColor="null";
+          if (validColors.contains(firstColor) && validColors.contains(secondColor) && validColors.contains(thirdColor)) {
+            decodeMain(band, firstColor, secondColor, thirdColor, fourthColor);
+          } else {
+            System.out.println("Enter valid colors only! list:\n"+validColors);
+          }
+        } else if (band==5) {
+          System.out.println("Valid colors: black, brown, red, orange, yellow, green, blue, violet, grey, white\n");
+          System.out.print("Enter the first band color=>");
+          String a1=st.nextLine();
+          System.out.print("Enter the Second band color=>");
+          String a2=st.nextLine();
+          System.out.print("Enter the third band color=>");
+          String a3=st.nextLine();
+          System.out.print("Enter the fourth band color=> ");
+          String a4=st.nextLine();
+          firstColor=a1.toLowerCase();
+          secondColor=a2.toLowerCase();
+          thirdColor=a3.toLowerCase();
+          fourthColor=a4.toLowerCase();
+          if (validColors.contains(firstColor) && validColors.contains(secondColor) && validColors.contains(thirdColor) && validColors.contains(fourthColor)) {
+            decodeMain(band, firstColor, secondColor, thirdColor, fourthColor);
+          } else {
+            System.out.println("Enter valid colors only! list:\n"+validColors);
+          }
+        } //else {
+          //System.out.println("Choose whether 4 or 5 and nothing else!");
+        //}
       }
     }
   }
@@ -496,4 +540,137 @@ public static int graphicalViewFourColorBand(String firstColor, String secondCol
               []=>firstColor.toUpperCase*/
     return 0;
   }
+  //decoder program
+  public static int decodeMain(int band, String firstColor, String secondColor, String thirdColor, String fourthColor) {
+    String colorName="null"; //here i have given value "null" just to initialise the variables
+    int firstDigit, secondDigit;
+    if (band == 4) {
+      colorName=firstColor;
+      firstDigit=significantDigit(colorName);
+      colorName=secondColor;
+      secondDigit=significantDigit(colorName);
+      colorName=thirdColor;
+      String thirdDigit=multiplierDigit(colorName);
+      //calculations------->
+      String var1=Integer.toString(firstDigit);
+      String var2=Integer.toString(secondDigit);
+      String var3=var1+var2;
+      long significantDigit=Long.valueOf(var3);
+      if (thirdDigit == "p" || thirdDigit == "pp") {
+        System.out.println("[!] Sorry but this program is under developement!");
+      } else {
+        long multiplierDigit=Long.valueOf(thirdDigit);
+        String resistorValue=resistorValue(significantDigit, multiplierDigit);
+        graphicalViewFourColorBand(firstColor, secondColor, thirdColor);
+        System.out.println("RESISTOR VALUE: "+resistorValue);
+      }
+    } else if (band == 5) {
+      colorName=firstColor;
+      firstDigit=significantDigit(colorName);
+      colorName=secondColor;
+      secondDigit=significantDigit(colorName);
+      colorName=thirdColor;
+      int thirdDigit=significantDigit(colorName);
+      colorName=fourthColor;
+      String fourthDigit=multiplierDigit(colorName);
+      //calculation------->
+      String var1=Integer.toString(firstDigit);
+      String var2=Integer.toString(secondDigit);
+      String var3=Integer.toString(thirdDigit);
+      String var4=var1+var2+var3;
+      long significantDigit=Long.valueOf(var4);
+      if (fourthDigit == "p" || fourthDigit == "pp") {
+        System.out.println("[!] sorry but this program is under development");
+      } else {
+        long multiplierDigit=Long.valueOf(fourthDigit);
+        String resistorValue=resistorValue(significantDigit, multiplierDigit);
+        graphicalViewFiveColorBand(firstColor, secondColor, thirdColor, fourthColor);
+        System.out.println("RESISTOR VALUE: "+resistorValue);
+      }
+    }
+    return 0;
+  }
+  public static int significantDigit(String colorName) {
+    int digitValue=0; // here i have given value 0 just to initialise the variable digitValue.
+    if (colorName == "black") {
+      digitValue=0;
+    } else if (colorName == "brown") {
+      digitValue=1;
+    } else if (colorName == "red") {
+      digitValue=2;
+    } else if (colorName == "orange") {
+      digitValue=3;
+    } else if (colorName == "yellow") {
+      digitValue=4;
+    } else if (colorName == "green") {
+      digitValue=5;
+    } else if (colorName == "blue") {
+      digitValue=6;
+    } else if (colorName == "violet") {
+      digitValue=7;
+    } else if (colorName == "grey") {
+      digitValue=8;
+    } else if (colorName == "white") {
+      digitValue=9;
+    }
+    return digitValue;
+  }
+  public static String multiplierDigit(String colorName) {
+    String digitValue="0"; //here i have given value 0 just to initialise the variable digitvalue.
+    if (colorName == "black") {
+      digitValue="1";
+    } else if (colorName == "brown") {
+      digitValue="10";
+    } else if (colorName == "red") {
+      digitValue="100";
+    } else if (colorName == "orange") {
+      digitValue="1000";
+    } else if (colorName == "yellow") {
+      digitValue="10000";
+    } else if (colorName == "green") {
+      digitValue="100000";
+    } else if (colorName == "blue") {
+      digitValue="1000000";
+    } else if (colorName == "violet") {
+      digitValue="10000000";
+    } else if (colorName == "silver") {
+      digitValue="pp";
+    } else if (colorName == "gold") {
+      digitValue="p";
+    }
+    return digitValue;
+  }
+  public static String resistorValue(long significantDigit, long multiplierDigit) {
+    String resistorValue="a"; //i have given here the value "a" just to initialise the variable
+    long var1;//similaraly here :)
+    if (multiplierDigit == 1) {
+      var1=significantDigit*multiplierDigit;
+      resistorValue=Long.toString(var1)+" Ohm";
+    } else if (multiplierDigit == 10) {
+      var1=significantDigit*multiplierDigit;
+      resistorValue=Long.toString(var1)+" Ohm";
+    } else if (multiplierDigit == 100) {
+      var1=significantDigit*multiplierDigit;
+      resistorValue=Long.toString(var1)+" Ohm";
+    } else if (multiplierDigit == 1000) {
+      var1=significantDigit;
+      resistorValue=Long.toString(var1)+" KiloOhm";
+    } else if (multiplierDigit == 10000) {
+      var1=significantDigit*10;
+      resistorValue=Long.toString(var1)+" KiloOhm";
+    } else if (multiplierDigit == 100000) {
+      var1=significantDigit*100;
+      resistorValue=Long.toString(var1)+" KiloOhm";
+    } else if (multiplierDigit == 1000000) {
+      var1=significantDigit;
+      resistorValue=Long.toString(var1)+" MegaOhm";
+    } else if (multiplierDigit == 10000000) {
+      var1=significantDigit*10;
+      resistorValue=Long.toString(var1)+" MegaOhm";
+    } else {
+      System.out.println("[!] Out of range!");
+    }
+    return resistorValue;
+  }
 }
+
